827f04aa5c0efaeda8e4bbc8abbdc0bb35f14c72
Joshua Grosso <jgrosso1@asu.edu>
2017-09-14 11:32:26 -0700

    CLIENT: Fix notification listening

--- client/client/ViewController.swift
+++ client/client/ViewController.swift
@@ -10,45 +10,21 @@ import Cocoa
 import SocketIO
 
 class ViewController: NSViewController {
+  private var socket: SocketIOClient
+
   required init?(coder: NSCoder) {
-    super.init(coder: coder)
-
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(currentApplicationChanged),
-      name: .NSWorkspaceDidActivateApplication,
-      object: nil
-    )
-  }
-
-  deinit {
-    NotificationCenter.default.removeObserver(self)
-  }
-
-  private var socket: SocketIOClient!
-
-  @objc private func currentApplicationChanged() {
-    print("Current application changed!")
-    emitCurrentApplication()
-  }
-
-  private func emitCurrentApplication() {
-    if let currentApplication = NSWorkspace.shared().frontmostApplication?.localizedName {
-      socket.emit("current-application", currentApplication)
-    }
-  }
-
-  private func runScript(_ source: String) {
-    if let script = NSAppleScript(source: source) {
-      var error: NSDictionary?
-      script.executeAndReturnError(&error)
-    }
-  }
-
-  override func viewDidLoad() {
     socket = SocketIOClient(
       socketURL: URL(string: "http://localhost:3000")!,
       config: [.log(true), .compress]
+    )
+
+    super.init(coder: coder)
+
+    NSWorkspace.shared().notificationCenter.addObserver(
+      self,
+      selector: #selector(currentApplicationChanged),
+      name: NSNotification.Name.NSWorkspaceDidActivateApplication,
+      object: nil
     )
 
     socket.on(clientEvent: .connect) { data, ack in
@@ -65,5 +41,27 @@ class ViewController: NSViewController {
       self.runScript("tell application \"System Events\" to keystroke \(data[0])")
     }
     socket.connect()
+  }
+
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+
+  @objc private func currentApplicationChanged(_ notification: NSNotification) {
+    print("Current application changed!")
+    emitCurrentApplication()
+  }
+
+  private func emitCurrentApplication() {
+    if let currentApplication = NSWorkspace.shared().frontmostApplication?.localizedName {
+      socket.emit("current-application", currentApplication)
+    }
+  }
+
+  private func runScript(_ source: String) {
+    if let script = NSAppleScript(source: source) {
+      var error: NSDictionary?
+      script.executeAndReturnError(&error)
+    }
   }
 }
